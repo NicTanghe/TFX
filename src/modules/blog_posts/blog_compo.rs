9d594d235e5882c46ce6530384dc6fd027dd95b5
NicTanghe <nicolaas.tanghe@gmail.com>
2024-10-31 16:22:23 +0100

    added post filtering

--- src/modules/blog_posts/blog_compo.rs
+++ src/modules/blog_posts/blog_compo.rs
@@ -4,6 +4,7 @@ use serde::{Serialize,Deserialize};
 
 use markdown::to_html as markdown_to_html;
 
+ use web_sys::wasm_bindgen::JsCast;
 //use crate::modules::blog_posts::blog_fn::*; 
 
 //use leptos_markdown::*;
@@ -19,25 +20,83 @@ pub struct Post {
 }   
     
 
+use leptos::{component, view, IntoView, ReadSignal, WriteSignal, create_signal, Signal};
+use leptos_router::A;
+
+
+
 #[component]
 pub fn PostList(posts: ReadSignal<Vec<Post>>) -> impl IntoView {
+    // Signal to store the currently selected tag for filtering
+    let (selected_tag, set_selected_tag) = create_signal(None::<String>);
+
+    // Collect unique tags once, outside the `view!` macro
+    let unique_tags = {
+        // Create a HashSet to store unique tags
+        let mut tags = std::collections::HashSet::new();
+        for post in posts.get().iter() {
+            tags.extend(post.tags.iter().cloned());
+        }
+        // Convert HashSet into a Vec for sorted collection of tags
+        tags.into_iter().collect::<Vec<String>>()
+    };
+
+    //this is absolutly great. it whould probably be less complicated and better ux if it just
+    //shows all the tags as buttons that you can toggle to filter. safe this somewhere though as you wil need it
+
+    println!("{:?}", unique_tags); // Debug: Check contents of `unique_tags`
+
     view! {
         <div class="big_void"></div>
         <div class="post-list">
             <h3>"Posts"</h3>
-            <div class="post-list-posts">
-                {move || posts.get().into_iter().map(|post| {
-                    // Use the title from the Post struct for href and display
-                    let href = format!("/blog/{}", post.title.to_lowercase());
-                    view! {
-                        <A href={href}>{&post.title}</A>
+            
+            // Tag filter section
+            <div class="tag-filter">
+                <label>"Filter by Tag: "</label>
+                <select on:input=move |event| {
+                    // Safely cast the event target to `HtmlSelectElement` and get its value
+                    if let Some(target) = event.target() {
+                        if let Ok(select_element) = target.dyn_into::<web_sys::HtmlSelectElement>() {
+                            let selected = select_element.value();
+                            set_selected_tag.set(if selected.is_empty() { None } else { Some(selected) });
+                        }
                     }
-                }).collect_view()}
+                }> // ok look at being able to the list open and selecting multiple entries instead
+                   // of using buttons. although a scrollable list of toggle buttons is probably what you
+                   // want.
+                    <option value="">"All"</option>
+                    {
+                        // Generate <option> elements for each unique tag
+                        unique_tags.iter()
+                            .map(|tag| view! {
+                                <option value={tag.clone()}>{tag}</option>
+                            })
+                            .collect_view()
+                    }
+                </select>
             </div>
+
+            // Posts list
+            <div class="post-list-posts">
+                {
+                    move || posts.get().into_iter()
+                        .filter(|post| selected_tag.get().as_deref().map_or(true, |tag| post.tags.contains(&tag.to_string())))
+                        .map(|post| {
+                            let href = format!("/blog/{}", post.title.to_lowercase());
+                            view! {
+                                <A href={href}>{&post.title}</A>
+                            }
+                        })
+                        .collect_view()
+                }
+            </div>
+
             <Outlet/>
         </div>
     }
 }
+
 
 
 
